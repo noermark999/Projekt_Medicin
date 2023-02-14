2073a4c7f23155b3684f85a177b6a57dcd0ee579
louisemogensen <louisemogensen2@gmail.com>
2023-02-14 09:43:35 +0100

    hva så

--- src/ordination/ordination/DagligSkaev.java
+++ src/ordination/ordination/DagligSkaev.java
@@ -1,12 +1,45 @@
 package ordination.ordination;
 
+import java.time.LocalDate;
 import java.time.LocalTime;
+import java.util.ArrayList;
 
-public class DagligSkaev {
+public class DagligSkaev extends Ordination{
+
+    //Linkattribut til Dosis
+    private ArrayList<Dosis> doser = new ArrayList<>();
+
+    //Constructor
     // TODO
+    public DagligSkaev(LocalDate startDen, LocalDate slutDen) {
+        super(startDen, slutDen);
+    }
 
+    //Vedligeholder forbindelsen til Dosis
+    public ArrayList<Dosis> getDoser() {
+        return new ArrayList<>(doser);
+    }
 
     public void opretDosis(LocalTime tid, double antal) {
         // TODO
+        Dosis dosis = new Dosis(tid, antal);
+        doser.add(dosis);
     }
+
+    //Nedståendde er implementeret via Java
+    @Override
+    public double samletDosis() {
+        return 0;
+    }
+
+    @Override
+    public double doegnDosis() {
+        return 0;
+    }
+
+    @Override
+    public String getType() {
+        return null;
+    }
+
 }
