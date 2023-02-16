a30f596cafd71f974fb846872039bef90ac5ca94
Tobias <eaatoat@students.eaaa.dk>
2023-02-16 10:14:15 +0100

    DagligFastTest ændret

--- test/ordination/ordination/DagligFastTest.java
+++ test/ordination/ordination/DagligFastTest.java
@@ -1,10 +1,13 @@
 package ordination.ordination;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class DagligFastTest {
 
-    @org.junit.jupiter.api.Test
+    @Test
     void createDosis() {
+
     }
 }
